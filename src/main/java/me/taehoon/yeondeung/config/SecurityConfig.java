b46ac5ee5f27d32c28d540e56e7b9f3a3ede2ebc
taehoondev030 <taehoondev030@gmail.com>
2024-12-02 18:30:40 +0900

    success create JWT

--- src/main/java/me/taehoon/yeondeung/config/SecurityConfig.java
+++ src/main/java/me/taehoon/yeondeung/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package me.taehoon.yeondeung.config;
 
+import me.taehoon.yeondeung.jwt.JWTUtil;
 import me.taehoon.yeondeung.jwt.LoginFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -18,10 +19,12 @@ public class SecurityConfig {
 
     // AuthenticationManager가 인자로 받을 AuthenticationConfiguraion 객체 생성자 주입
     private final AuthenticationConfiguration authenticationConfiguration;
+    private final JWTUtil jwtUtil;
 
-    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration, JWTUtil jwtUtil) {
 
         this.authenticationConfiguration = authenticationConfiguration;
+        this.jwtUtil = jwtUtil;
     }
 
     // AuthenticationManager Bean 등록
@@ -60,7 +63,7 @@ public class SecurityConfig {
 
         // 로그인 필터 등록
         http
-                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class);
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration), jwtUtil), UsernamePasswordAuthenticationFilter.class);
 
         // 세션 설정
         http
